51f36514c14a3c9097974bf2470b025f1a9cdfab
bittercounter90 <andromaxac33@gmail.com>
2026-09-05 21:21:51 +0700

    pembuatan database menggunakan JS

---
+++ database.js
@@ -0,0 +1,31 @@
+const databaseVTuber = {
+    "asep": {
+        name: "ASEP KNALPOT",
+        date: "Minggu, 19 April 2026",
+        time: "20:00 WIB (GMT+7)",
+        qrWaiting: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=WaitingRoomAsep",
+        qrForm: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=GoogleFormAsep"
+    },
+    "piponly": {
+        name: "PIPONELY",
+        date: "Minggu, 19 April 2026",
+        time: "20:00 WIB (GMT+7)",
+        qrWaiting: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=WaitingRoomPiponely",
+        qrForm: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=GoogleFormPiponely"
+    },
+    "budi": {
+        name: "BUDI GAMING",
+        date: "Senin, 20 April 2026",
+        time: "19:00 WIB (GMT+7)",
+        qrWaiting: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=WaitingRoomBudi",
+        qrForm: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=GoogleFormBudi"
+    },
+    "rucus": {
+        name: "RucusLiveID",
+        date: "Senin, 20 April 2026",
+        time: "19:00 WIB (GMT+7)",
+        qrWaiting: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=WaitingRoomBudi",
+        qrForm: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=GoogleFormBudi"
+    }
+    // Jika mau nambah orang baru, CUKUP TAMBAH DI FILE INI SAJA!
+};
